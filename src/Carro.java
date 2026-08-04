488c5030ba8fd2784bb66d28425ccca43136a8fc
Breno0409-Byte <brenobr0409@gmail.com>
2026-08-04 16:09:48 -0400

    Commit-4

---
+++ src/Carro.java
@@ -0,0 +1,25 @@
+
+
+public class Carro {
+
+    String modelo;
+    String marca;
+    int ano;
+
+    void exibirInformacoes() {
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Marca: " + marca);
+        System.out.println("Ano: " + ano);
+    }
+
+    public static void main(String[] args) {
+
+        Carro carro1 = new Carro();
+
+        carro1.modelo = "Civic";
+        carro1.marca = "Honda";
+        carro1.ano = 2022;
+
+        carro1.exibirInformacoes();
+    }
+}
